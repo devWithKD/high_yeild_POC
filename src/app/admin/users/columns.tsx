506a21f01154648dba75b9ac706d7e767987fcab
SarthakPatil <125047555+SarthakPatil11@users.noreply.github.com>
2024-11-10 13:55:29 +0530

    [Admin Panel] UserTable and UserAddForm

---
+++ src/app/admin/users/columns.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import * as React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Badge } from "@/components/ui/badge";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// const data: User[] = [
+//     {
+//         id: "m5gr84i9",
+//         name: "Shubham",
+//         role: "accountant",
+//         email: "ken99@yahoo.com",
+//     },
+//     {
+//         id: "3u1reuv4",
+//         name: "Kedar",
+//         role: "admin",
+//         email: "Abe45@gmail.com",
+//     },
+//     {
+//         id: "derv1ws0",
+//         name: "Sarthak",
+//         role: "investor",
+//         email: "Monserrat44@gmail.com",
+//     },
+//     {
+//         id: "5kma53ae",
+//         name: "Suyash",
+//         role: "support",
+//         email: "Silas22@gmail.com",
+//     },
+//     {
+//         id: "bhqecj4p",
+//         name: "Sakshi",
+//         role: "investor",
+//         email: "carmella@hotmail.com",
+//     },
+// ];
+
+export type User = {
+    id: string;
+    name: string;
+    role: "super_admin" | "admin" | "accountant" | "investor" | "support";
+    email: string;
+};
+
+export const columns: ColumnDef<User>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) =>
+                    table.toggleAllPageRowsSelected(!!value)
+                }
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "role",
+        header: "role",
+        cell: ({ row }) => {
+            return <Badge variant="outline">{row.getValue("role")}</Badge>;
+        },
+    },
+    {
+        accessorKey: "name",
+        header: "name",
+        cell: ({ row }) => (
+            <div className="capitalize">{row.getValue("name")}</div>
+        ),
+    },
+    {
+        accessorKey: "email",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === "asc")
+                    }
+                >
+                    Email
+                    <ArrowUpDown />
+                </Button>
+            );
+        },
+        cell: ({ row }) => (
+            <div className="lowercase">{row.getValue("email")}</div>
+        ),
+    },
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            const payment = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() =>
+                                navigator.clipboard.writeText(payment.id)
+                            }
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>
+                            View payment details
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        },
+    },
+];
